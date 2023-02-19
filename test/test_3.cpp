b3eec6fb34d0fa6538d5cdbd881936b8ebaaa4b9
maksasj <radioboos@gmail.com>
2023-02-19 19:23:01 +0200

    brute force test

---
+++ test/test_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "test.h"
+
+using namespace thoth;
+
+int main() {
+    TEST_CASE {
+        Integer a("0");
+        Integer b("1");
+
+
+        for(int i = 0; i < 2000000; ++i) {
+            ensure(a.toString() == std::to_string(i));
+            
+            a.plus(b);
+        }
+    }
+
+    return 0;
+}
